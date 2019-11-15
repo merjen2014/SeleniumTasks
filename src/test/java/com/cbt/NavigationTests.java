1cb46fe03ccabf4a95411f0f0972d1f2d9481e8e
merjen2014 <m.rejepowa1988@gmail.com>
2019-11-14 23:15:38 -0800

    cbt

--- src/test/java/com/cbt/NavigationTests.java
+++ src/test/java/com/cbt/NavigationTests.java
@@ -1,4 +1,39 @@
 package com.cbt;
 
+import org.openqa.selenium.WebDriver;
+import utils2.BrowserFactory;
+
 public class NavigationTests {
+    public static void main(String[] args) {
+
+        WebDriver driver = BrowserFactory.getDriver("chrome");
+
+        driver.get("https://google.com");
+
+        String expected = driver.getTitle();
+
+        driver.navigate().to("https://etsy.com");
+        String expected2 = driver.getTitle();
+        driver.navigate().back();
+
+        String actual = driver.getTitle();
+
+        if (actual.equals(expected)) {
+            System.out.println("Passed");
+        } else {
+            System.out.println("Failed");
+        }
+
+        driver.navigate().forward();
+        String actual2 = driver.getTitle();
+
+        if (actual2.equals(expected2)) {
+            System.out.println("Passed");
+        } else {
+            System.out.println("Failed");
+        }
+
+
+        driver.close();
+    }
 }
